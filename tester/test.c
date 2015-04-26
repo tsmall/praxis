cc1eac521d207d28979517ead21753646df182a2
Tom Small <tsmall3@gmail.com>
2015-04-26 09:57:42 -0400

    tester: Communicate between processes via stdin and stdout

--- tester/test.c
+++ tester/test.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sysexits.h>
 #include <unistd.h>
 
 int main(int argc, char** argv) {
-	int pipefds[2];
-	pipe(pipefds);
+	int stdoutpipe[2], stdinpipe[2];
+	pipe(stdoutpipe);
+	pipe(stdinpipe);
 
 	pid_t childpid = fork();
 	if (childpid == -1) {
@@ -15,32 +17,47 @@ int main(int argc, char** argv) {
 
 	if (childpid == 0) {
 		// child
-		close(pipefds[0]);
-		printf("child: spawned\n");
+		close(stdoutpipe[0]);
+		close(stdinpipe[1]);
 
-		// send stdout and stderr to the pipe
-		dup2(pipefds[1], 1);
-		dup2(pipefds[1], 2);
+		// Redirect stdin, stdout, and stderr to use pipes
+		// so parent process can read them.
+		dup2(stdinpipe[0], STDIN_FILENO);
+		dup2(stdoutpipe[1], STDOUT_FILENO);
+		dup2(stdoutpipe[1], STDERR_FILENO);
 
-		printf("child: stdout\n");
+		char buffer[1024];
+		fgets(buffer, sizeof(buffer), stdin);
+		buffer[strcspn(buffer, "\n")] = '\0';
+		printf("child: received <%s>\n", buffer);
+
 		sleep(1);
-		fprintf(stderr, "child: stderr\n");
-		sleep(1);
+
+		close(stdoutpipe[1]);
+		close(stdinpipe[0]);
 	}
 	else {
 		// parent
-		close(pipefds[1]);
-		printf("parent: still running\n");
+		close(stdoutpipe[1]);
+		close(stdinpipe[0]);
 
-		char buffer[20];
-		while (read(pipefds[0], buffer, 15) > 0) {
+		FILE* instream = fdopen(stdinpipe[1], "w");
+		fprintf(instream, "Hi!\n");
+		fflush(instream);
+
+		char buffer[1024];
+		FILE* outstream = fdopen(stdoutpipe[0], "r");
+		while (!feof(outstream) && !ferror(outstream) && fgets(buffer, sizeof(buffer), outstream)) {
 			printf("parent: got value from child\n");
-			printf("parent: %s\n", buffer);
+			printf("parent: %s", buffer);
 		}
 
 		int status;
 		waitpid(childpid, &status, 0);
 		printf("parent: child process ended\n");
+
+		close(stdoutpipe[0]);
+		close(stdinpipe[1]);
 	}
 
 	return 0;
