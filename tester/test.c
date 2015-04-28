a625a6d92141cd6c095ff7344dd2a14f1fcf5793
Tom Small <tsmall3@gmail.com>
2015-04-28 08:41:45 -0400

    tester: Add testing logic and reorganize

--- tester/test.c
+++ tester/test.c
@@ -1,10 +1,16 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sysexits.h>
 #include <unistd.h>
 
-int main(int argc, char** argv) {
+#define READ 0
+#define WRITE 1
+
+typedef enum { false, true } bool;
+
+void runproc(const char* path, char* const argv[], const char* input, char* out, int outsize) {
 	int stdoutpipe[2], stdinpipe[2];
 	pipe(stdoutpipe);
 	pipe(stdinpipe);
@@ -17,42 +23,57 @@ int main(int argc, char** argv) {
 
 	if (childpid == 0) {
 		// child
-		close(stdoutpipe[0]);
-		close(stdinpipe[1]);
+		close(stdoutpipe[READ]);
+		close(stdinpipe[WRITE]);
 
 		// Redirect stdin, stdout, and stderr to use pipes
 		// so parent process can read them.
-		dup2(stdinpipe[0], STDIN_FILENO);
-		dup2(stdoutpipe[1], STDOUT_FILENO);
-		dup2(stdoutpipe[1], STDERR_FILENO);
+		dup2(stdinpipe[READ], STDIN_FILENO);
+		dup2(stdoutpipe[WRITE], STDOUT_FILENO);
+		dup2(stdoutpipe[WRITE], STDERR_FILENO);
 
-		char* argv[] = { "cat", "-" };
-		execvp(argv[0], argv);
+		execv(path, argv);
 	}
 	else {
 		// parent
-		close(stdoutpipe[1]);
-		close(stdinpipe[0]);
+		close(stdoutpipe[WRITE]);
+		close(stdinpipe[READ]);
 
-		FILE* instream = fdopen(stdinpipe[1], "w");
-		fprintf(instream, "Hi!\n");
+		FILE* instream = fdopen(stdinpipe[WRITE], "w");
+		fputs(input, instream);
 		fflush(instream);
 		fclose(instream);
 
-		char buffer[1024];
-		FILE* outstream = fdopen(stdoutpipe[0], "r");
-		while (!feof(outstream) && !ferror(outstream) && fgets(buffer, sizeof(buffer), outstream)) {
+		/* TODO: Handle multiple line output */
+		FILE* outstream = fdopen(stdoutpipe[READ], "r");
+		while (!feof(outstream) && !ferror(outstream) && fgets(out, outsize, outstream)) {
 			printf("parent: got value from child\n");
-			printf("parent: %s", buffer);
+			printf("parent: %s", out);
 		}
 
 		int status;
 		waitpid(childpid, &status, 0);
 		printf("parent: child process ended\n");
 
-		close(stdoutpipe[0]);
-		close(stdinpipe[1]);
+		// fclose(instream);
+		close(stdoutpipe[READ]);
+		close(stdinpipe[WRITE]);
 	}
+}
+
+bool runtest(const char* path, char* const argv[], const char* input, const char* expected) {
+	char buffer[1024];
+	runproc(path, argv, input, buffer, sizeof(buffer));
+	return strcmp(expected, buffer) == 0;
+}
+
+int main(int argc, char* argv[]) {
+	char* childargv[] = { "cat", "-", NULL };
+	char* input = "Hi!\n";
+	char* expected = "Hi!\n";
+
+	bool successful = runtest("/bin/cat", childargv, input, expected);
+	printf("result: %d\n", successful);
 
 	return 0;
 }
